6d10721efedd5287f55dfab2fddf2a23726461ba
Rachana8888 <krachana8888@gmail.com>
2023-12-10 15:38:48 -0500

    ConsumingWebApi To MVC

---
+++ SaggiWebConsume/Controllers/RoleViewController.cs
@@ -0,0 +1,110 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SaggiWebConsume.Models;
+
+namespace SaggiWebConsume.Controllers
+{
+    public class RoleViewController : Controller
+    {
+        Uri baseAddress = new Uri("http://localhost:8082/api");
+        private readonly HttpClient _client;
+        public RoleViewController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<RoleView> roleViews = new List<RoleView>();
+            HttpResponseMessage response = _client.GetAsync(baseAddress+"/roles").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                roleViews = JsonConvert.DeserializeObject<List<RoleView>>(data);
+
+            }
+
+           
+        
+            return View(roleViews);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(RoleView view)
+        {
+            try
+            {
+                String data = JsonConvert.SerializeObject(view);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = _client.PostAsync(_client.BaseAddress + "/roles", content).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["successMessage"] = "Role Added";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] =ex.Message;
+
+                return View();
+            }
+
+
+              
+            return View(); 
+        }
+        [HttpGet]
+        public IActionResult Edit(int id) 
+        {
+            try
+            {
+                RoleView roleView = new RoleView();
+                HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/roles/"+id).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = response.Content.ReadAsStringAsync().Result;
+                    roleView = JsonConvert.DeserializeObject<RoleView>(data);
+                }
+                return View(roleView);
+            }
+            catch (Exception ex)
+            {
+
+                TempData["errorMessge"] = ex.Message;
+                return View();
+            }      
+         }
+        [HttpPost]
+        public IActionResult Edit(RoleView view)
+        {
+            try
+            {
+                string data = JsonConvert.SerializeObject(view);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = _client.PutAsync(_client.BaseAddress + "/roles", content).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["successMessage"] = "Role Updated";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                TempData["errorMessge"] = ex.Message;
+                return View();
+            }
+            return View();
+
+        }
+
+
+    }
+}
---
+++ SaggiWebConsume/Models/RoleView.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SaggiWebConsume.Models
+{
+    public class RoleView
+    {
+        /*public RoleView()
+        {
+            Users = new HashSet<User>();
+        }*/
+        [Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RoleId { get; set; }
+        //[Column(TypeName = "nvarchar(25)")]
+
+        public string RoleName { get; set; }
+        //public virtual ICollection<User> Users { get; set; }
+    }
+}
